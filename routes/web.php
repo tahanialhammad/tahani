b405e3c6106eed5bc6b6cc36aaa6f3edaebd9396
Tahani <t.alhammad@linq5.nl>
2023-08-05 12:17:04 +0200

    add auth admin and users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Auth admin
+Route::middleware('is_admin')->group(function () {
+    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+});
 
+ // Auth users
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [UserController::class, 'users'])->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
